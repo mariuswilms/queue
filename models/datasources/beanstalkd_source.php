7104d93d8a530c3417f6e9695ab472a4fa1707a7
Shawn Murphy <seguer@gmail.com>
2012-10-12 15:27:06 +1100

    Merge davidperrson/queue:master

--- models/datasources/beanstalkd_source.php
+++ models/datasources/beanstalkd_source.php
@@ -2,7 +2,7 @@
 /**
  * Beanstalkd Source File
  *
- * Copyright (c) 2009 David Persson
+ * Copyright (c) 2009-2012 David Persson
  *
  * Distributed under the terms of the MIT License.
  * Redistributions of files must retain the above copyright notice.
@@ -12,12 +12,12 @@
  *
  * @package    queue
  * @subpackage queue.models.datasources
- * @copyright  2009 David Persson <davidpersson@gmx.de>
+ * @copyright  2009-2012 David Persson <davidpersson@gmx.de>
  * @license    http://www.opensource.org/licenses/mit-license.php The MIT License
  * @link       http://github.com/davidpersson/queue
  */
 App::import('Core', 'DataSource');
-App::import('Lib', 'Queue.BeanstalkdSocket');
+require_once dirname(dirname(dirname(__FILE__))) . '/libs/beanstalk/src/Socket/Beanstalk.php';
 
 /**
  * Beanstalkd Source Class
@@ -55,7 +55,7 @@ class BeanstalkdSource extends DataSource {
 		parent::__construct();
 		$this->setConfig($config);
 		$this->fullDebug = Configure::read('debug') > 1;
-		$this->connection = new BeanstalkdSocket($this->config);
+		$this->connection = new Socket_Beanstalk($this->config);
 		$this->connected =& $this->connection->connected;
 		$this->connect();
 	}
@@ -390,20 +390,27 @@ class BeanstalkdSource extends DataSource {
 
 	function describe($model) {
 	}
-
+/**
+ * Logs a new query. Will automatically truncate already logged so at any time
+ * there are no more than 200 messages logged.
+ *
+ * @param string $method
+ * @param array $params
+ * @return void
+ */
 	function logQuery($method, $params) {
 		$this->_queriesCnt++;
 		$this->_queriesTime += $this->took;
-    $this->_queriesLog[] = array(
-      'query' => $method,
-      'error' => $this->error,
-      'took' => $this->took,
-      'affected' => 0,
-      'numRows' => 0
-    );
-    if (count($this->_queriesLog) > $this->_queriesLogMax) {
-      array_pop($this->_queriesLog);
-    }
+		if (count($this->_queriesLog) >= 200) {
+			array_shift($this->_queriesLog);
+		}
+		array_push($this->_queriesLog, array(
+			'query' => $method,
+			'error' => $this->error,
+			'took' => $this->took,
+			'affected' => 0,
+			'numRows' => 0
+		));
 	}
 
 	function lastError() {
--- models/job.php
+++ models/job.php
@@ -2,7 +2,7 @@
 /**
  * Job Model File
  *
- * Copyright (c) 2009 David Persson
+ * Copyright (c) 2009-2012 David Persson
  *
  * Distributed under the terms of the MIT License.
  * Redistributions of files must retain the above copyright notice.
@@ -12,10 +12,12 @@
  *
  * @package    queue
  * @subpackage queue.models
- * @copyright  2009 David Persson <davidpersson@gmx.de>
+ * @copyright  2009-2012 David Persson <davidpersson@gmx.de>
  * @license    http://www.opensource.org/licenses/mit-license.php The MIT License
  * @link       http://github.com/davidpersson/queue
  */
+
+App::import('Core', 'ConnectionManager');
 
 /**
  * Job Model Class
@@ -26,11 +28,34 @@
 class Job extends QueueAppModel {
 
 /**
- * Database configuration to use
+ * Database configuration to use. Before using this model be sure to
+ * define at least a minimal connection in your database.php file:
+ * {{{
+ *   // ...
+ *	'queue' => array('datasource' => 'beanstalkd')
+ * }}}
  *
  * @var string
- * @access public
  */
 	var $useDbConfig = 'queue';
+
+/**
+ * Check to see if queue is online and accepts jobs.
+ *
+ * @param array $cached Pass `true` to enable cached responses.
+ * @return boolean
+ */
+	function online($cached = false) {
+		static $status;
+
+		if ($cached && isset($status)) {
+			return $status;
+		}
+		$Manager = ConnectionManager::getInstance();
+		@$DataSource = $Manager->getDataSource($this->useDbConfig);
+
+		return $status = $DataSource && $DataSource->isConnected();
+	}
 }
+
 ?>
